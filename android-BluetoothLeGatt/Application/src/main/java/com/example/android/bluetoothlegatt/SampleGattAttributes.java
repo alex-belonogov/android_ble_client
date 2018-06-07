8e0512601e2dcd208b3cc8bc7f68e93e9b08f263
Hassan <hchaudhary@carepredict.com>
2018-06-07 00:57:22 -0400

    remove demo services

--- android-BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ android-BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -23,17 +23,9 @@ import java.util.HashMap;
  */
 public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
-    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
 
     static {
-        // Sample Services.
-        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
-        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
-        // Sample Characteristics.
-        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
-        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
-
         //easily identify Tempo service -- must be lower case
         attributes.put("000000ff-0000-1000-8000-00805f9b34fb", "CarePredict Tempo Service");
         attributes.put("0000ff01-0000-1000-8000-00805f9b34fb", "Read Write Indication Characteristic");
@@ -41,6 +33,6 @@ public class SampleGattAttributes {
 
     public static String lookup(String uuid, String defaultName) {
         String name = attributes.get(uuid);
-        return name == null ? defaultName : name;
+        return name;    //return null, if not found
     }
 }
